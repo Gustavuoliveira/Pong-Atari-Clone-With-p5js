2c1d928de87c8b8860454f9fcd479b45ba2ff334
Gustavo Dell Anhol Oliveira <gustavo.dellanhol@gmail.com>
2022-08-29 17:32:58 -0300

    Conclusão do Projeto
    Foi criado o placar do jogo com uma fonte customizada, implementado os sons para os eventos e modificado a IA do inimigo com a dificuldade adaptável

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-//Canvas Variables
+//Canvas variables
 let canvas_width = 700;
 let canvas_height = 500;
 
+//Score Variables
+let pong_font;
+let font_size = 70;
+let enemy_score = 00;
+let player_score = 00;
+
+//Sound Variables
+let sound_victory = new Audio('library/assets/victory.mp3');
+let sound_paddle = new Audio('library/assets/paddle.mp3');
+let sound_score = new Audio('library/assets/score.mp3');
+
 //Ball variables
-let ball_speed = 0;
+let ball_speed = 1;
 let ball_size = 15;
 let step_x = 5;
-let step_y = 3;
+let step_y = 5;
 let ball_x = canvas_width/2;
 let ball_y = canvas_height/2;
 
@@ -19,29 +30,50 @@ let player_y = canvas_height/2 - player_height/2;
 //Enemy variables
 let enemy_x = player_width/2;
 let enemy_y = canvas_height/2 - player_height/2;
-let enemy_speed = 1;
-let difficulty = 3;
+let enemy_step = 4;
+let enemy_difficulty = 3;
 
 //Checking collisions
 let collide_player = false;
 let collide_enemy = false;
 
+function preload(){
+  pong_font = loadFont('https://www.mattlag.com/bitfonts/bit5x3.ttf');
+}
+
 function setup() {
   createCanvas(canvas_width, canvas_height);
+
 }
 
 function draw() {
   background('black');
 
+  update_score();
+
   ellipse(ball_x, ball_y, ball_size);                      // Drawing the ball
   rect(player_x, player_y, player_width, player_height);   // Drawing the player
   rect(enemy_x, enemy_y, player_width, player_height);     // Drawing the enemy
+
 
   ball_movement();
   ball_collisions();
   enemy_movement();
   player_movement();
+}
 
+function update_score(){
+  if(enemy_score == 10 || player_score == 10){
+    enemy_score = 0;
+    player_score = 0;
+    sound_victory.play();
+  }
+  
+  fill('white');
+  textFont(pong_font);
+  textSize(font_size);
+  text(String(enemy_score).padStart(2,'0'), canvas_width/4 - font_size/2, 100);
+  text(String(player_score).padStart(2,'0'), canvas_width*3/4 - font_size/2, 100);
 }
 
 function player_movement(){
@@ -55,9 +87,11 @@ function player_movement(){
 }
 
 function enemy_movement(){  
-  enemy_y += enemy_speed;
-  if(ball_y > enemy_y + (player_height/2)) enemy_speed = difficulty;
-  if(ball_y < enemy_y + (player_height/2)) enemy_speed = -difficulty;
+  enemy_y = enemy_y + enemy_step
+  
+  //The enemy moves according to the position of the ball
+  if(ball_y > enemy_y + (player_height/2)) enemy_step = enemy_step = enemy_difficulty;
+  if(ball_y < enemy_y + (player_height/2)) enemy_step = enemy_step = -enemy_difficulty;
 
   //Enemy collision with the top
   if(enemy_y < 0) enemy_y = 0
@@ -66,9 +100,8 @@ function enemy_movement(){
 }
 
 function ball_movement(){
-  ball_x += step_x //+ ball_speed;
-  print(ball_speed);
-  ball_y += step_y;
+  ball_x = ball_x + step_x;
+  ball_y = ball_y + step_y;
 }
 
 function ball_collisions(){
@@ -78,26 +111,37 @@ function ball_collisions(){
   //Ball collision with the player
   collide_player = collideRectCircle(player_x,player_y,player_width,player_height,ball_x,ball_y,ball_size);
   if (collide_player == true){
-    step_x *= -1;
+    if(step_x < 0) step_x -= .1; else step_x += .1;
+    step_x *= -1; //Reverse the ball position
+    sound_paddle.play();
   }
   //Ball collision with the enemy
   collide_enemy = collideRectCircle(enemy_x,enemy_y,player_width,player_height,ball_x,ball_y,ball_size);
   if (collide_enemy == true){
-    step_x *= -1;
+    if(step_x < 0) step_x -= .5; else step_x += .5;
+    step_x *= -1; //Reverse the ball position
+    sound_paddle.play();
   }
 
   //Ball collision with the sides
-  if(ball_x + ball_size/2 > width){
+  if(ball_x + ball_size/2 > width){ //Right wall
+    enemy_score++;
+    sound_score.play();
+    step_x = 5; //Slows the ball velocity
+    enemy_difficulty = random([3,4,4.5,5]); //Sets the position randomly
     step_y *= random([1,-1]);
-    ball_speed = 0;
+    //Return the ball to the center
     ball_x = canvas_width/2;
     ball_y = canvas_height/2;
   }
 
-  if (ball_x - ball_size/2 < 0){
-    difficulty += .5;
+  if (ball_x - ball_size/2 < 0){ //Left wall
+    player_score++;
+    sound_score.play();
+    step_x = 5; //Slows the ball velocity
+    enemy_difficulty = random([3,4,4.5,5]); //Sets the position randomly
     step_y *= random([1,-1]);
-    ball_speed = 0;
+    //Return the ball to the center
     ball_x = canvas_width/2;
     ball_y = canvas_height/2;
   }
